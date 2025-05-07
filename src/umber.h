40e4a9dcd1b46126bb5e87e7595bbc15fdf44a4f
Aymeric Wibo <obiwac@gmail.com>
2025-05-07 19:51:38 +0200

    formatting: Standardize with all the other AQUA stuff

--- src/umber.h
+++ src/umber.h
@@ -4,9 +4,9 @@
 #undef UMBER_COMPONENT // Force user to define this after header inclusion.
 
 #if !defined(__UMBER__)
-#define __UMBER__
+# define __UMBER__
 
-#include <stdint.h>
+# include <stdint.h>
 
 typedef enum {
 	UMBER_LVL_FATAL,
@@ -19,16 +19,15 @@ typedef enum {
 
 void umber_log(umber_lvl_t const lvl, char const* const component, char const* const path, char const* const func, uint32_t const line, char const* const msg);
 
-__attribute__((__format__(__printf__, 6, 0)))
-void umber_vlog(umber_lvl_t const lvl, char const* const component, char const* const path, char const* const func, uint32_t const line, char const* const fmt, ...);
+__attribute__((__format__(__printf__, 6, 0))) void umber_vlog(umber_lvl_t const lvl, char const* const component, char const* const path, char const* const func, uint32_t const line, char const* const fmt, ...);
 
 // Helper macros.
 
-#define LOG_FATAL(...)   umber_vlog(UMBER_LVL_FATAL,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...)   umber_vlog(UMBER_LVL_ERROR,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define LOG_WARN(...)    umber_vlog(UMBER_LVL_WARN,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define LOG_SUCCESS(...) umber_vlog(UMBER_LVL_SUCCESS, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define LOG_INFO(...)    umber_vlog(UMBER_LVL_INFO,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define LOG_VERBOSE(...) umber_vlog(UMBER_LVL_VERBOSE, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_FATAL(...) umber_vlog(UMBER_LVL_FATAL, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_ERROR(...) umber_vlog(UMBER_LVL_ERROR, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_WARN(...) umber_vlog(UMBER_LVL_WARN, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_SUCCESS(...) umber_vlog(UMBER_LVL_SUCCESS, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_INFO(...) umber_vlog(UMBER_LVL_INFO, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+# define LOG_VERBOSE(...) umber_vlog(UMBER_LVL_VERBOSE, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
 #endif
--- tests/filter/filter.c
+++ tests/filter/filter.c
@@ -1,15 +1,15 @@
 #include <umber.h>
 
 void FUNC_NAME(void) {
-	#define UMBER_COMPONENT COMP_A
+#define UMBER_COMPONENT COMP_A
 	LOG_VERBOSE("%s", SPEC_A) // line 5
-	#undef UMBER_COMPONENT
+#undef UMBER_COMPONENT
 
-	#define UMBER_COMPONENT COMP_B		
+#define UMBER_COMPONENT COMP_B
 	LOG_VERBOSE("%s", SPEC_B) // line 9
-	#undef UMBER_COMPONENT
+#undef UMBER_COMPONENT
 
-	#define UMBER_COMPONENT COMP_C
+#define UMBER_COMPONENT COMP_C
 	LOG_VERBOSE("%s", SPEC_C) // line 13
 }
 
