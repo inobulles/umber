b66b95eae4a1726bf66c428abf523718ade69841
Aymeric Wibo <obiwac@gmail.com>
2025-05-07 21:05:04 +0200

    tests: Use semicolons

--- tests/filter/filter.c
+++ tests/filter/filter.c
@@ -2,15 +2,15 @@
 
 void FUNC_NAME(void) {
 #define UMBER_COMPONENT COMP_A
-	LOG_VERBOSE("%s", SPEC_A) // line 5
+	LOG_VERBOSE("%s", SPEC_A); // line 5
 #undef UMBER_COMPONENT
 
 #define UMBER_COMPONENT COMP_B
-	LOG_VERBOSE("%s", SPEC_B) // line 9
+	LOG_VERBOSE("%s", SPEC_B); // line 9
 #undef UMBER_COMPONENT
 
 #define UMBER_COMPONENT COMP_C
-	LOG_VERBOSE("%s", SPEC_C) // line 13
+	LOG_VERBOSE("%s", SPEC_C); // line 13
 }
 
 int main(void) {
--- tests/levels/levels.c
+++ tests/levels/levels.c
@@ -2,12 +2,12 @@
 #define UMBER_COMPONENT COMPONENT
 
 void FUNC_NAME(void) {
-	LOG_FATAL(TEST_FMT, TEST_ARG)   // line 5
-	LOG_ERROR(TEST_FMT, TEST_ARG)   // line 6
-	LOG_WARN(TEST_FMT, TEST_ARG)    // line 7
-	LOG_SUCCESS(TEST_FMT, TEST_ARG) // line 8
-	LOG_INFO(TEST_FMT, TEST_ARG)    // line 9
-	LOG_VERBOSE(TEST_FMT, TEST_ARG) // line 10
+	LOG_FATAL(TEST_FMT, TEST_ARG);   // line 5
+	LOG_ERROR(TEST_FMT, TEST_ARG);   // line 6
+	LOG_WARN(TEST_FMT, TEST_ARG);    // line 7
+	LOG_SUCCESS(TEST_FMT, TEST_ARG); // line 8
+	LOG_INFO(TEST_FMT, TEST_ARG);    // line 9
+	LOG_VERBOSE(TEST_FMT, TEST_ARG); // line 10
 }
 
 int main(void) {
