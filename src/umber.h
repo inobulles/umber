03aa80dfe89c9ca5782692092b569fb56c14a4ff
Aymeric Wibo <obiwac@gmail.com>
2025-05-07 19:50:17 +0200

    umber.h: Require semicolons after helper macros

--- src/umber.h
+++ src/umber.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2022-2025 Aymeric Wibo
 
-#undef UMBER_COMPONENT // force user to define this after header inclusion
+#undef UMBER_COMPONENT // Force user to define this after header inclusion.
 
 #if !defined(__UMBER__)
 #define __UMBER__
@@ -22,13 +22,13 @@ void umber_log(umber_lvl_t const lvl, char const* const component, char const* c
 __attribute__((__format__(__printf__, 6, 0)))
 void umber_vlog(umber_lvl_t const lvl, char const* const component, char const* const path, char const* const func, uint32_t const line, char const* const fmt, ...);
 
-// helper macros
+// Helper macros.
 
-#define LOG_FATAL(...)   umber_vlog(UMBER_LVL_FATAL,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
-#define LOG_ERROR(...)   umber_vlog(UMBER_LVL_ERROR,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
-#define LOG_WARN(...)    umber_vlog(UMBER_LVL_WARN,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
-#define LOG_SUCCESS(...) umber_vlog(UMBER_LVL_SUCCESS, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
-#define LOG_INFO(...)    umber_vlog(UMBER_LVL_INFO,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
-#define LOG_VERBOSE(...) umber_vlog(UMBER_LVL_VERBOSE, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__);
+#define LOG_FATAL(...)   umber_vlog(UMBER_LVL_FATAL,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...)   umber_vlog(UMBER_LVL_ERROR,   UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...)    umber_vlog(UMBER_LVL_WARN,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_SUCCESS(...) umber_vlog(UMBER_LVL_SUCCESS, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...)    umber_vlog(UMBER_LVL_INFO,    UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_VERBOSE(...) umber_vlog(UMBER_LVL_VERBOSE, UMBER_COMPONENT, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
 #endif
